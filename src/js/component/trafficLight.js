1618ba53dece554c5b12f9f5e2f7769fd0297ffe
AnanyAbellop <padronalbany@gmail.com>
2020-08-02 20:43:30 +0000

    Segunda actualizacion

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -3,11 +3,23 @@ import React from "react";
 //create your first component
 export function TrafficLight() {
 	return (
-		<div className="row">
-			<div className="lights col-auto">
-				<input id="red" type="radio" />
-				<input id="yellow" type="radio" />
-				<input id="green" type="radio" />
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="lights col-auto">
+					<input id="red" type="radio" />
+				</div>
+			</div>
+
+			<div className="row justify-content-center">
+				<div className="lights col-auto">
+					<input id="yellow" type="radio" />
+				</div>
+			</div>
+
+			<div className="row justify-content-center">
+				<div className="lights col-auto">
+					<input id="green" type="radio" />
+				</div>
 			</div>
 		</div>
 	);
