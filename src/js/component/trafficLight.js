76a3afaa48e024b316c609b5a17eec39bfd8bdc8
AnanyAbellop <padronalbany@gmail.com>
2020-08-10 15:53:01 +0000

    Final commit

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -1,25 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
 //create your first component
 export function TrafficLight() {
+	const [color, setColor] = useState("red");
+
 	return (
-		<div className="container">
+		<div className="container ligths">
 			<div className="row justify-content-center">
-				<div className="lights col-auto">
-					<input id="red" type="radio" />
-				</div>
+				<div
+					className={`lights col-auto ${color == "red" &&
+						"light-shadow"}`}
+					id="red"
+					style={{ opacity: color == "red" ? 1 : 0.5 }}
+					onClick={e => setColor("red")}
+
+					// var new ={ ...color};
+					// let newColor ={...new[0]};
+					// newColor.boxShadow="0px 0px 9px 4px #747DE8";
+					// new[0] = newColor;
+					// setColor({new});
+					// // // "0px 0px 9px 4px #747DE8";
+					// // setColor(color[0].boxShadow, newColor);
+					// // //alert(color[0].borderColor);
+					// // setColor((color[1].borderColor = ""));
+					// // setColor((color[2].borderColor = ""));
+				/>
 			</div>
 
 			<div className="row justify-content-center">
-				<div className="lights col-auto">
-					<input id="yellow" type="radio" />
-				</div>
+				<div
+					className={`lights col-auto ${color == "yellow" &&
+						"light-shadow"}`}
+					style={{ opacity: color == "yellow" ? 1 : 0.5 }}
+					onClick={e => setColor("yellow")}
+					id="yellow"
+				/>
 			</div>
 
 			<div className="row justify-content-center">
-				<div className="lights col-auto">
-					<input id="green" type="radio" />
-				</div>
+				<div
+					className={`lights col-auto ${color == "green" &&
+						"light-shadow"}`}
+					style={{ opacity: color == "green" ? 1 : 0.5 }}
+					onClick={e => setColor("green")}
+					id="green"
+				/>
 			</div>
 		</div>
 	);
